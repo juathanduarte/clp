f0212d1bc7787eaf2a64a52d6d7ff89524937cb9
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-10-16 19:39:23 -0300

    fractal gerando pelo python um pouco random

--- Fractal de Mandelbrot/interface.py
+++ Fractal de Mandelbrot/interface.py
@@ -2,123 +2,173 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from ctypes import *
 
 mandelbrotGen = CDLL("./libMandelbrot.so")
- 
-mandelbrotGen.mandelbrot()
+
+# mandelbrotGen.mandelbrot()
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(797, 600)
+        
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName("horizontalLayout")
+        
         self.left_container = QtWidgets.QFrame(self.centralwidget)
         self.left_container.setMaximumSize(QtCore.QSize(300, 16777215))
         self.left_container.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.left_container.setFrameShadow(QtWidgets.QFrame.Raised)
         self.left_container.setObjectName("left_container")
+        
         self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.left_container)
         self.verticalLayout_4.setObjectName("verticalLayout_4")
+        
         self.frame = QtWidgets.QFrame(self.left_container)
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
+        
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.frame)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        
         self.title_Fractal = QtWidgets.QLabel(self.frame)
         self.title_Fractal.setObjectName("title_Fractal")
+        
         self.horizontalLayout_2.addWidget(self.title_Fractal)
+        
         self.verticalLayout_4.addWidget(self.frame)
+        
         self.frame_2 = QtWidgets.QFrame(self.left_container)
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
+        
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.frame_2)
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        
         self.toolBox = QtWidgets.QToolBox(self.frame_2)
         self.toolBox.setObjectName("toolBox")
+        
         self.page_membros = QtWidgets.QWidget()
         self.page_membros.setGeometry(QtCore.QRect(0, 0, 246, 370))
         self.page_membros.setObjectName("page_membros")
+        
         self.gridLayout = QtWidgets.QGridLayout(self.page_membros)
         self.gridLayout.setObjectName("gridLayout")
+        
         self.avatar_Juathan = QtWidgets.QLabel(self.page_membros)
         self.avatar_Juathan.setMaximumSize(QtCore.QSize(150, 150))
         self.avatar_Juathan.setText("")
         self.avatar_Juathan.setPixmap(QtGui.QPixmap("interface/avatares/juatsAvatar.png"))
         self.avatar_Juathan.setScaledContents(True)
         self.avatar_Juathan.setObjectName("avatar_Juathan")
+        
         self.gridLayout.addWidget(self.avatar_Juathan, 0, 0, 1, 1)
+        
         self.name_Juathan = QtWidgets.QLabel(self.page_membros)
         self.name_Juathan.setObjectName("name_Juathan")
+        
         self.gridLayout.addWidget(self.name_Juathan, 1, 0, 1, 1)
+        
         self.avatar_Lucas = QtWidgets.QLabel(self.page_membros)
         self.avatar_Lucas.setMaximumSize(QtCore.QSize(150, 150))
         self.avatar_Lucas.setText("")
         self.avatar_Lucas.setPixmap(QtGui.QPixmap("interface/avatares/lucasAvatar.png"))
         self.avatar_Lucas.setScaledContents(True)
         self.avatar_Lucas.setObjectName("avatar_Lucas")
+        
         self.gridLayout.addWidget(self.avatar_Lucas, 2, 0, 1, 1)
+        
         self.name_Lucas = QtWidgets.QLabel(self.page_membros)
         self.name_Lucas.setObjectName("name_Lucas")
+        
         self.gridLayout.addWidget(self.name_Lucas, 3, 0, 1, 1)
+        
         self.toolBox.addItem(self.page_membros, "")
+        
         self.page_sobre = QtWidgets.QWidget()
         self.page_sobre.setGeometry(QtCore.QRect(0, 0, 260, 358))
         self.page_sobre.setObjectName("page_sobre")
+        
         self.label = QtWidgets.QLabel(self.page_sobre)
         self.label.setGeometry(QtCore.QRect(10, 0, 241, 351))
         self.label.setObjectName("label")
+        
         self.toolBox.addItem(self.page_sobre, "")
+        
         self.horizontalLayout_3.addWidget(self.toolBox)
+        
         self.verticalLayout_4.addWidget(self.frame_2)
+        
         self.horizontalLayout.addWidget(self.left_container)
+        
         self.right_container = QtWidgets.QFrame(self.centralwidget)
         self.right_container.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.right_container.setFrameShadow(QtWidgets.QFrame.Raised)
         self.right_container.setObjectName("right_container")
+        
         self.verticalLayout = QtWidgets.QVBoxLayout(self.right_container)
         self.verticalLayout.setObjectName("verticalLayout")
+        
         self.header_frame = QtWidgets.QFrame(self.right_container)
         self.header_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.header_frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.header_frame.setObjectName("header_frame")
+        
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.header_frame)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
+        
         self.title_clp = QtWidgets.QLabel(self.header_frame)
         self.title_clp.setObjectName("title_clp")
+        
         self.verticalLayout_2.addWidget(self.title_clp)
+        
         self.verticalLayout.addWidget(self.header_frame)
+        
         self.main_frame = QtWidgets.QFrame(self.right_container)
+        
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.main_frame.sizePolicy().hasHeightForWidth())
+        
         self.main_frame.setSizePolicy(sizePolicy)
         self.main_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.main_frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.main_frame.setObjectName("main_frame")
+        
         self.label_4 = QtWidgets.QLabel(self.main_frame)
         self.label_4.setGeometry(QtCore.QRect(50, 40, 62, 17))
         self.label_4.setObjectName("label_4")
+        
         self.button_GerarFractal = QtWidgets.QPushButton(self.main_frame)
         self.button_GerarFractal.setGeometry(QtCore.QRect(10, 320, 435, 150))
         self.button_GerarFractal.setMinimumSize(QtCore.QSize(250, 150))
         self.button_GerarFractal.setMaximumSize(QtCore.QSize(435, 150))
         self.button_GerarFractal.setObjectName("button_GerarFractal")
+        self.button_GerarFractal.clicked.connect(mandelbrotGen.mandelbrot)
+        
         self.verticalLayout.addWidget(self.main_frame)
+        
         self.footer_frame = QtWidgets.QFrame(self.right_container)
         self.footer_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.footer_frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.footer_frame.setObjectName("footer_frame")
+        
         self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.footer_frame)
         self.verticalLayout_3.setObjectName("verticalLayout_3")
+        
         self.title_footer = QtWidgets.QLabel(self.footer_frame)
         self.title_footer.setObjectName("title_footer")
+        
         self.verticalLayout_3.addWidget(self.title_footer)
+        
         self.verticalLayout.addWidget(self.footer_frame)
+        
         self.horizontalLayout.addWidget(self.right_container)
+        
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -136,10 +186,9 @@ class Ui_MainWindow(object):
         self.toolBox.setItemText(self.toolBox.indexOf(self.page_sobre), _translate("MainWindow", "Sobre"))
         self.title_clp.setText(_translate("MainWindow", "CLP - Conceitos de Linguagens de Programação"))
         self.label_4.setText(_translate("MainWindow", "Options:"))
-        self.button_GerarFractal.setText(_translate("MainWindow", "Gerar Fractal"))
+        self.button_GerarFractal.setText(_translate("MainWindow", "Gerar Fractal de Mandelbrot aleatório"))
         self.title_footer.setText(_translate("MainWindow", "Todos direitos reservados"))
-
-
+    
 if __name__ == "__main__":
     import sys
     
--- Fractal de Mandelbrot/mandelbrot.c
+++ Fractal de Mandelbrot/mandelbrot.c
@@ -1,69 +1,111 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-int mandelbrot(){
+int main(){
+    srand(time(NULL));
+
     /* screen ( integer) coordinate */
     int iX,iY;
-    const int iXmax = 800; 
-    const int iYmax = 800;
+    const int iXmax = 1280; 
+    const int iYmax = 720;
+
     /* world ( double) coordinate = parameter plane*/
     double Cx,Cy;
-    const double CxMin=-2.5;
-    const double CxMax=1.5;
-    const double CyMin=-2.0;
-    const double CyMax=2.0;
-    /* */
+    const double CxMin = rand() % (int)(0 + 1 - (-2)) + (-2); 
+    const double CyMin = rand() % (int)(0 + 1 - (-2)) + (-2);
+    const double CxMax = rand() % (int)(2 + 1 - (0)) + (0);
+    const double CyMax = rand() % (int)(2 + 1 - (0)) + (0);
+    // const double CxMin = -1;
+    // const double CxMax = 0;
+    // const double CyMin = -1;
+    // const double CyMax = 1;
+
+    printf("CxMin = %f, CxMax = %f, CyMin = %f, CyMax = %f", CxMin, CxMax, CyMin, CyMax);
+
     double PixelWidth=(CxMax-CxMin)/iXmax;
     double PixelHeight=(CyMax-CyMin)/iYmax;
+
     /* color component ( R or G or B) is coded from 0 to 255 */
     /* it is 24 bit color RGB file */
     const int MaxColorComponentValue=255; 
+    
     FILE * fp;
-    char *filename="new1.png";
+    char *filename="new4.png";
+
     char *comment="# ";/* comment should start with # */
+
     static unsigned char color[3];
+
     /* Z=Zx+Zy*i  ;   Z0 = 0 */
     double Zx, Zy;
     double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
+
     /*  */
     int Iteration;
     const int IterationMax=3000;
+
     /* bail-out value , radius of circle ;  */
-    const double EscapeRadius=2;
+    const double EscapeRadius= 2;
     double ER2=EscapeRadius*EscapeRadius;
+
     /*create new file,give it a name and open it in binary mode  */
     fp= fopen(filename,"wb"); /* b -  binary mode */
     /*write ASCII header to the file*/
+
     fprintf(fp,"P6\n %s\n %d\n %d\n %d\n",comment,iXmax,iYmax,MaxColorComponentValue);
+
+    int rInteriorMandelbrot = rand() % MaxColorComponentValue;
+    int gInteriorMandelbrot = rand() % MaxColorComponentValue;
+    int bInteriorMandelbrot = rand() % MaxColorComponentValue;
+
+    printf("\n\nRGB INTERIOR= %d, %d, %d\n\n", rInteriorMandelbrot, gInteriorMandelbrot, bInteriorMandelbrot);
+
+    int rExteriorMandelbrot = rand() % MaxColorComponentValue;
+    int gExteriorMandelbrot = rand() % MaxColorComponentValue;
+    int bExteriorMandelbrot = rand() % MaxColorComponentValue;
+
+    printf("RGB EXTERIOR= %d, %d, %d\n\n", rExteriorMandelbrot, gExteriorMandelbrot, bExteriorMandelbrot);
+
+    do{
+        rInteriorMandelbrot = rand() % MaxColorComponentValue;
+        gInteriorMandelbrot = rand() % MaxColorComponentValue;
+        bInteriorMandelbrot = rand() % MaxColorComponentValue;
+    } while (rInteriorMandelbrot - rExteriorMandelbrot < 100 && gInteriorMandelbrot - gExteriorMandelbrot < 100 && bInteriorMandelbrot - bExteriorMandelbrot < 100);
+
+   printf("\n\nRGB INTERIOR DEPOIS DO WHILE= %d, %d, %d\n\n", rInteriorMandelbrot, gInteriorMandelbrot, bInteriorMandelbrot);
+
     /* compute and write image data bytes to the file*/
-    for(iY=0;iY<iYmax;iY++){
-        Cy=CyMin + iY*PixelHeight;
+    for(iY = 0; iY < iYmax; iY++){
+        Cy = CyMin + iY * PixelHeight;
 
-        if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
+        if (fabs(Cy) < PixelHeight/2) Cy = 0.0; /* Main antenna */
 
-        for(iX=0;iX<iXmax;iX++){         
-            Cx=CxMin + iX*PixelWidth;
+        for(iX = 0; iX < iXmax; iX++){         
+            Cx = CxMin + iX * PixelWidth;
             /* initial value of orbit = critical point Z= 0 */
             Zx=0.0;
             Zy=0.0;
             Zx2=Zx*Zx;
             Zy2=Zy*Zy;
             /* */
-            for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++){
-                Zy=2*Zx*Zy + Cy;
-                Zx=Zx2-Zy2 +Cx;
-                Zx2=Zx*Zx;
-                Zy2=Zy*Zy;
+            for (Iteration = 500; Iteration < IterationMax && ((Zx2+Zy2)<ER2);Iteration++){
+                Zy = (rand() % 3)*Zx*Zy + Cy;
+                // Zy = 2*Zx*Zy + Cy;
+                Zx = Zx2-Zy2 +Cx;
+                Zx2 = Zx*Zx;
+                Zy2 = Zy*Zy;
             };
             /* compute  pixel color (24 bit = 3 bytes) */
-            if (Iteration==IterationMax){ /*  interior of Mandelbrot set = black */
-                color[0]=0;
-                color[1]=0;
-                color[2]=0;                           
-            } else { /* exterior of Mandelbrot set = white */
-                color[0]=255; /* Red*/
-                color[1]=255;  /* Green */ 
-                color[2]=255;/* Blue */
+            if (Iteration == IterationMax){ /*  EXTERIOR of Mandelbrot set = black */
+                color[0] = rExteriorMandelbrot;
+                color[1] = gExteriorMandelbrot;  
+                color[2] = bExteriorMandelbrot;                        
+            } else { /* INTERIOR of Mandelbrot set = white */
+                color[0] = rInteriorMandelbrot;
+                color[1] = gInteriorMandelbrot;
+                color[2] = bInteriorMandelbrot;   
+                
             };
             /*write color to the file*/
             fwrite(color,1,3,fp);
